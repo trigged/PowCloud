bbc3886b7fdc2b4529a023fad0482360a88480e7
trigged <trigged@live.cn>
2014-07-21 15:26:24 +0800

    update plan

--- app/views/output/plan.php
+++ app/views/output/plan.php
@@ -1,7 +1,23 @@
 <html>
 <body>
-<table>
-    <?php var_dump($data); ?>
-</table>
+
+<?php
+if (isset($data)) {
+    foreach ($data as $property) {
+        $text = '';
+        foreach ($property as $value) {
+            $text .= $value . "   ";
+        }
+        if (!empty($text)) {
+            echo "<h3>$text</h3>";
+        }
+    }
+
+} else {
+    echo "<h4>您请求的方式有问题 请和管理员联系</h4>";
+}
+?>
+
+
 </body>
 </html>
