2719160ecbea1aa9eef0c3fa29ef3c5e50400248
trigged <trigged@live.cn>
2014-10-29 16:20:07 +0800

    add login page

--- app/views/cms/login.php
+++ app/views/cms/login.php
@@ -1,198 +1,36 @@
-<style>
-        /*@import url(http://fonts.googleapis.com/css?family=Montserrat:400,700);*/
-
-    html {
-        background: url(http://thekitemap.com/images/feedback-img.jpg) no-repeat;
-        background-size: cover;
-        height: 100%;
-    }
-
-    #feedback-page {
-        text-align: center;
-    }
-
-    #form-main {
-        width: 100%;
-        float: left;
-        padding-top: 0px;
-    }
-
-    #form-div {
-        background-color: rgba(72, 72, 72, 0.4);
-        padding-left: 35px;
-        padding-right: 35px;
-        padding-top: 35px;
-        padding-bottom: 50px;
-        width: 450px;
-        float: left;
-        left: 50%;
-        position: absolute;
-        margin-top: 30px;
-        margin-left: -260px;
-        -moz-border-radius: 7px;
-        -webkit-border-radius: 7px;
-    }
-
-    .feedback-input {
-        color: #3c3c3c;
-        font-family: Helvetica, Arial, sans-serif;
-        font-weight: 500;
-        font-size: 18px;
-        border-radius: 0;
-        line-height: 22px;
-        background-color: #fbfbfb;
-        padding: 13px 13px 13px 54px;
-        margin-bottom: 10px;
-        width: 100%;
-        -webkit-box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        -ms-box-sizing: border-box;
-        box-sizing: border-box;
-        border: 3px solid rgba(68, 26, 26, 1);
-    }
-
-    .feedback-input:focus {
-        background: #fff;
-        box-shadow: 0;
-        border: 3px solid #3498db;
-        color: #3498db;
-        outline: none;
-        padding: 13px 13px 13px 54px;
-    }
-
-    .focused {
-        color: #30aed6;
-        border: #30aed6 solid 3px;
-    }
-
-        /* Icons ---------------------------------- */
-    #name {
-        background-image: url(http://rexkirby.com/kirbyandson/images/name.svg);
-        background-size: 30px 30px;
-        background-position: 11px 8px;
-        background-repeat: no-repeat;
-    }
-
-    #name:focus {
-        background-image: url(http://rexkirby.com/kirbyandson/images/name.svg);
-        background-size: 30px 30px;
-        background-position: 8px 5px;
-        background-position: 11px 8px;
-        background-repeat: no-repeat;
-    }
-
-    #password {
-        background-image: url(http://rexkirby.com/kirbyandson/images/comment.svg);
-        background-size: 30px 30px;
-        background-position: 11px 8px;
-        background-repeat: no-repeat;
-    }
-
-    #password:focus {
-        background-image: url(http://rexkirby.com/kirbyandson/images/comment.svg);
-        background-size: 30px 30px;
-        background-position: 11px 8px;
-        background-repeat: no-repeat;
-    }
-
-    #comment {
-        background-image: url(http://rexkirby.com/kirbyandson/images/comment.svg);
-        background-size: 30px 30px;
-        background-position: 11px 8px;
-        background-repeat: no-repeat;
-    }
-
-    textarea {
-        width: 100%;
-        height: 150px;
-        line-height: 150%;
-        resize: vertical;
-    }
-
-    input:hover, textarea:hover,
-    input:focus, textarea:focus {
-        background-color: white;
-    }
-
-    #button-blue {
-        font-family: 'Montserrat', Arial, Helvetica, sans-serif;
-        float: left;
-        width: 100%;
-        border: #fbfbfb solid 4px;
-        cursor: pointer;
-        background-color: #3498db;
-        color: white;
-        font-size: 24px;
-        padding-top: 22px;
-        padding-bottom: 22px;
-        -webkit-transition: all 0.3s;
-        -moz-transition: all 0.3s;
-        transition: all 0.3s;
-        margin-top: -4px;
-        font-weight: 700;
-    }
-
-    #button-blue:hover {
-        background-color: rgba(0, 0, 0, 0);
-        color: #0493bd;
-    }
-
-    .ease {
-        width: 0px;
-        height: 74px;
-        background-color: #fbfbfb;
-        -webkit-transition: .3s ease;
-        -moz-transition: .3s ease;
-        -o-transition: .3s ease;
-        -ms-transition: .3s ease;
-        transition: .3s ease;
-    }
-
-    .submit:hover .ease {
-        width: 100%;
-        background-color: white;
-    }
-
-    @media only screen and (max-width: 580px) {
-        #form-div {
-            left: 3%;
-            margin-right: 3%;
-            width: 88%;
-            margin-left: 0;
-            padding-left: 3%;
-            padding-right: 3%;
-        }
-    }
-</style>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <title>Powserver</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content=""/>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <!-- css -->
+    <link href="<?php echo URL::asset('css/register_login.css'); ?>" rel="stylesheet">
+</head>
 <body>
-<div style="text-align:center;clear:both">
 
-</div>
-<div id="form-main">
-    <style>
-        #form-div {
-
-        }
-    </style>
-    <div id="form-div">
-
-
+<div class="register_login_bg">
+    <div class="rl_title"></div>
+    <div class="bg_login form_bg">
         <form action="<?php echo URL::action('LoginController@loginStore'); ?>" method="post">
-
-            <p class="name"> 用户名：
-                <input name="username" type="text"
-                       class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name"
-                       id="name"/>
-            </p>
-
-            <p class="password"> 密码:
-                <input name="password" type="password" class="feedback-input" id="password" placeholder="password"/>
-            </p>
-
-            <div class="submit">
-                <input type="submit" value="登陆" id="button-blue"/>
+            <div class="item">
+                <input type="text" name="username" placeholder="请输入账号"/>
             </div>
+            <div class="item">
+                <input type="password" name="password" placeholder="请输入密码"/>
+            </div>
+            <a href="#" class="fotget_password">忘记密码？</a>
+            <input type="submit" value="立即登录" class="submit_button"/>
         </form>
     </div>
-
+    <div class="login_footer clearfix">
+        <span><em>其他登录方式</em><a href="#" class="icon_sina"><img src="images/icon_sina.png"></a><a href="#"
+                                                                                                  class="icon_qq"><img
+                    src="images/icon_qq.png"></a></span>
+        <a href="#" class="free_reigster">免费注册</a>
+    </div>
+</div>
 </body>
+</html>
