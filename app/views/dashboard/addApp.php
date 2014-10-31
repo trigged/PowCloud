a1e3aa3b783d8b8dacb9608426a122a081fcda5c
luli <luli_kenny@sina.com>
2014-10-31 14:50:30 +0800

    make input sriking

--- app/views/dashboard/addApp.php
+++ app/views/dashboard/addApp.php
@@ -6,23 +6,23 @@
                   action="<?php echo URL::action('DashBoardController@storeApp') ?>" method="post">
                 <fieldset>
                     <legend>添加应用</legend>
-                    <div class="">
-                        <label for="name" class="control-label" style="margin-top:12px;">应用名称*:</label>
+                    <div class="form-group">
+                        <label for="name" class="control-label col-sm-3" >应用名称*:</label>
 
-                        <div class="controls inline">
+                        <div class="controls col-sm-3">
                         <input name="name" class="form-control" value="" type="text" placeholder="应用名称" id="name">
                         </div>
                     </div>
-                    <div class="">
-                        <label for="app_id" class="control-label" style="margin-top:12px;">信息&nbsp;:</label>
+                    <div class="form-group">
+                        <label for="app_id" class="control-label col-sm-3">信息&nbsp;:</label>
 
-                        <div class="controls inline">
+                        <div class="controls col-sm-3">
                         <input name="info" class="form-control" value="" type="text" placeholder="信息" id="info">
 
                         </div>
                     </div>
 
-                    <div class="form-actions">
+                    <div style="margin-left:330px;">
                         <button id="JS_Sub" class="btn btn-primary">提交</button>
                         <a href="javascript:void (0)" class="btn  btn-warning" onclick="history.back();">取消</a>
                     </div>
