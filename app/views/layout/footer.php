925cce302c3a5607f59ca509d4f025f6ddddb6e3
trigged <trigged@live.cn>
2014-11-03 10:37:06 +0800

    change sidebar height

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -26,12 +26,18 @@
 </script>
 <script>
     $(function () {
-//        var side_h = $(window).height();
-//        $(".sidebar-nav").height(side_h);
-//        $(window).on('resize', function () {
-//            var side_h = $(window).height();
-//            $(".sidebar-nav").height(side_h);
-//        });
+        var side_h = $(window).height();
+        var or_height = $(".sidebar-nav").height();
+        if (or_height < side_h) {
+            $(".sidebar-nav").height(side_h);
+        }
+        $(window).on('resize', function () {
+            var side_h = $(window).height();
+            var or_height = $(".sidebar-nav").height();
+            if (or_height < side_h) {
+                $(".sidebar-nav").height(side_h);
+            }
+        });
     });
 
 </script>
